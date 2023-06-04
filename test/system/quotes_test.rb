509b4c7160ece78050b9178ea3f5a1d48effb9db
tamashiro-syuta <syuta5825@gmail.com>
2023-06-04 15:10:59 +0900

    🧪 テストデータをセットし、他のCRUD処理のテストを追加

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -1,6 +1,12 @@
 require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
+
+  # テストデータをセット
+  setup do
+    @quote = quotes(:first)
+  end
+
   test "Creating a new quote" do
     # Quotesのindexページにアクセスしたとき "Quotes "というテキストを持つタイトルが表示される
     visit quotes_path
@@ -20,4 +26,34 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
+
+  test "Showing a quote" do
+    visit quotes_path
+    click_link @quote.name
+
+    assert_selector "h1", text: @quote.name
+  end
+
+  test "Updating a quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "Edit", match: :first
+    assert_selector "h1", text: "Edit quote"
+
+    fill_in "Name", with: "Updated quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
+  end
+
+  test "Destroying a quote" do
+    visit quotes_path
+    assert_text @quote.name
+
+    click_on "Delete", match: :first
+    assert_no_text @quote.name
+  end
+
 end
