8488a357770e827666c128b82d304c4c1eaabb12
tamashiro-syuta <syuta5825@gmail.com>
2023-06-04 17:22:19 +0900

    💡 アプリケーション全体でturbo driveを使わないパターンを追記

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// アプリケーション全体で Turbo Drive を無効化する場合の設定
+// import { Turbo } from "@hotwired/turbo-rails"
+// Turbo.session.drive = false
