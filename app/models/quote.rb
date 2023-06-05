329b0494ad1105b690c0512c27990d5dfa84f01a
tamashiro-syuta <syuta5825@gmail.com>
2023-06-05 23:14:24 +0900

    👔 quotesストリームを購読しているターゲットにquotesのidを持つ要素を追加して配信

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # createのたびに、quotes ストリームを購読しているユーザーにブロードキャストし、quotes というidを持つDOMノードにプリペンドするように指示
+  # broadcast_prepend_toメソッドは、Turboで定義されているメソッド
+  # quotes/_quote.html.erbパーシャルを、ターゲット「quotes」によって識別された対象をTurbo Streamフォーマットでレンダリングして追加
+  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
 end
