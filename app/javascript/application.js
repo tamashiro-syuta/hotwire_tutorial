2559c83f1790b57ab0026b09f732f33dd78212f2
tamashiro-syuta <syuta5825@gmail.com>
2023-06-12 08:57:32 +0900

    ⏪ 一時的にturboが効かないようにした

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,5 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 
 // アプリケーション全体で Turbo Drive を無効化する場合の設定
-// import { Turbo } from "@hotwired/turbo-rails"
-// Turbo.session.drive = false
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
